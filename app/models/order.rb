efa46ee2d9871bbf9f00f53d8c14a634d2829b67
ALEX SLYE <slyealex@live.com>
2016-04-21 14:52:07 -0400

    add index new and show views for making an order, tested and working:

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ActiveRecord::Base
+  belongs_to :user
 end
+
+# quantity method to decrement based on orders remaining
+# choose delivery method pick-up, delivery, public meet
+# time of completion, when dish is done
